323692f63f4c8ef0658336acc5b0f81112cdfb37
arnoudx6 <arnoud.stolk@ziggo.nl>
2026-08-17 23:13:22 +0200

    Worked on the base type

--- src/smallir/dac/definitions/base.py
+++ src/smallir/dac/definitions/base.py
@@ -1,9 +1,9 @@
 import re
-from enum import IntEnum
-from typing import ClassVar, Any
+from enum import IntEnum, StrEnum, auto
+from typing import Any, ClassVar
 from uuid import UUID
 
-from pydantic import BaseModel, Field, field_validator, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
 class Severity(IntEnum):
@@ -31,6 +31,16 @@ class Priority(IntEnum):
     MEDIUM = 2
     HIGH = 3
 
+
+class Status(StrEnum):
+    """Possible statuses for rules."""
+
+    EXPERIMENTAL = auto()
+    TESTING = auto()
+    PRODUCTION = auto()
+    DEPRECATED = auto()
+
+
 # Models
 class Model(BaseModel):
     """The base model for all classes in this file."""
@@ -45,24 +55,33 @@ class Model(BaseModel):
 
     def to_dict(self) -> dict:
         """Convert the model to a dictionary."""
-        return self.model_dump(exclude_none=True)
+        return self.model_dump()
 
-    def to_schema_dict(self) -> dict[str, Any]:
+    def schema_to_dict(self) -> dict[str, Any]:
         """Dump the JSON Schema of the model as a dictionary."""
         return self.model_json_schema()
+
 
 class Action(Model):
     """Actions that can be executed by the rule."""
 
-    enabled: bool = Field(description="Whether the action is enabled.")
+    enabled: bool = Field(default=False, description="Whether the action is enabled.")
     name: str = Field(description="The name of the action.")
 
 
 class Suppression(Model):
     """Suppression settings for the rule."""
 
-    enabled: bool = Field(description="Whether the suppression is enabled.")
+    enabled: bool = Field(default=False, description="Whether the suppression is enabled.")
     time: str = Field(description="The time for the suppression.")
+
+class Source(Model):
+    """The source of the rule."""
+    path: str = Field(description="The path to the rule.")
+
+class aaaa(Model):
+    ticket: str = Field(description="The ticket for the rule.")
+
 
 
 class Search(Model):
@@ -71,11 +90,21 @@ class Search(Model):
     platform: str = Field(description="The target platform for the search.")
     query: str = Field(description="The query for the search.")
     lookback_start: str = Field(description="The start time for the lookback period.")
-    lookback_end: str = Field(description="The end time for the lookback period.")
-    index_start: str = Field(description="The start time for the index period.")
-    index_end: str = Field(description="The end time for the index period.")
-    suppression: Suppression = Field(description="The suppression for the search.")
-    action: list[str] = Field(description="The list of actions for the search.")
+    lookback_end: str | None = Field(
+        default=None, description="The end time for the lookback period."
+    )
+    index_start: str | None = Field(
+        default=None, description="The start time for the index period."
+    )
+    index_end: str | None = Field(
+        default=None, description="The end time for the index period."
+    )
+    suppression: Suppression | None = Field(
+        default=None, description="The suppression for the search."
+    )
+    actions: list[Action] = Field(
+        default_factory=list, description="The list of actions for the search."
+    )
 
 
 class Metadata(Model):
@@ -100,25 +129,19 @@ class Metadata(Model):
     enabled: bool = Field(description="Whether the rule is enabled.")
     id: str = Field(description="The internal identifier for the rule.")
     name: str = Field(description="The name of the rule.")
-    severity: Severity = Field(
-        description="The severity of the rule."
-    )
-    confidence: Confidence = Field(
-        description="The confidence of the rule."
-    )
-    priority: Priority = Field(
-        description="The priority of generated alerts."
-    )
+    description: str = Field(description="The description of the rule.")
+    severity: Severity = Field(description="The severity of the rule.")
+    confidence: Confidence = Field(description="The confidence of the rule.")
+    priority: Priority = Field(description="The priority of generated alerts.")
     mitre_techniques: list[str] = Field(
-        description="The MITRE techniques for the rule."
+        min_length=1, description="The MITRE techniques for the rule."
     )
-    labels: list[str] = Field(
-        min_length=1, description="The labels for the rule."
-    )
+    labels: list[str] = Field(min_length=1, description="The labels for the rule.")
 
 
 class Definition(Model):
     """The Definition container combining all data for a rule."""
+    source: Source = Field(description="The source of the rule.")
 
     metadata: Metadata
     searches: list[Search] = Field(
